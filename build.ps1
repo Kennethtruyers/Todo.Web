411e8f5b2d9cdfd74c4e2570221dfc3d9c17aa7e
Kenneth Truyers <truyers.kenneth@gmail.com>
2017-03-08 23:53:34 +0100

    fixup

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@ cls
 . '.\functions.ps1'
 
 $invokeBuild = (Get-ChildItem('.\packages\Invoke-Build*\tools\Invoke-Build.ps1')).FullName | Sort-Object $_ | Select -Last 1
+& $invokeBuild $args -File tasks.ps1
