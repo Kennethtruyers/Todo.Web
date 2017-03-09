63078bdc9ef1b38b3d0d54253c1a95154c237cea
Kenneth Truyers <truyers.kenneth@gmail.com>
2017-03-09 01:46:35 +0100

    Add Clean Task

---
+++ functions.ps1
@@ -0,0 +1,17 @@
+function Get-SolutionProjects
+{
+	Add-Type -Path (${env:ProgramFiles(x86)} + '\Reference Assemblies\Microsoft\MSBuild\v14.0\Microsoft.Build.dll')
+	
+	$solutionFile = (Get-ChildItem('*.sln')).FullName | Select -First 1
+	$solution = [Microsoft.Build.Construction.SolutionFile] $solutionFile
+	
+	return $solution.ProjectsInOrder | 
+		Where-Object {$_.ProjectType -eq 'KnownToBeMSBuildFormat'} |
+        ForEach-Object {
+        @{
+			Path = $_.AbsolutePath;
+			Name = $_.ProjectName;
+			Directory = "$(Split-Path -Path $_.AbsolutePath -Resolve)"
+        }
+    }	
+}
--- tasks.ps1
+++ tasks.ps1
@@ -1,7 +1,22 @@
-Task init {
-	Write-Host "Hello world!"
-}
+param(
+    $outputDirectory = (property outputDirectory "artifacts")
+)
 
-Task init2{
-	Write-Host "Hello world 2!"
+$absoluteOutputDirectory= "$((Get-Location).Path)\$outputDirectory"
+$projects = Get-SolutionProjects
+
+task Clean {
+	if((Test-Path $absoluteOutputDirectory))
+	{
+		Write-Host "Cleaning artifacts directory $absoluteOutputDirectory"
+		Remove-Item "$absoluteOutputDirectory" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+	}
+	New-Item $absoluteOutputDirectory -ItemType Directory | Out-Null
+
+	$projects | 
+		ForEach-Object {
+			Write-Host "Cleaning bin and obj folders for $($_.Directory)"
+			Remove-Item "$($_.Directory)\bin" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+			Remove-Item "$($_.Directory)\obj" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+		}
 }
