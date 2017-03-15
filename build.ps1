93179152cb37afbf4e708249ffbc5eab3d87da8e
Kenneth Truyers <truyers.kenneth@gmail.com>
2017-03-16 00:39:40 +0100

    Enable NuGet restore

--- build.ps1
+++ build.ps1
@@ -1,5 +1,18 @@
 cls
 
+$nugetPath = "$env:LOCALAPPDATA\NuGet\NuGet.exe"
+
+if (!(Get-Command NuGet -ErrorAction SilentlyContinue) -and !(Test-Path $nugetPath)) {
+	Write-Host 'Downloading NuGet.exe'
+	(New-Object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $nugetPath)
+} 
+
+if (Test-Path $nugetPath) { 
+	Set-Alias NuGet (Resolve-Path $nugetPath)
+} 
+Write-Host 'Restoring NuGet packages'
+NuGet restore
+
 . '.\functions.ps1'
 
 $invokeBuild = (Get-ChildItem('.\packages\Invoke-Build*\tools\Invoke-Build.ps1')).FullName | Sort-Object $_ | Select -Last 1
